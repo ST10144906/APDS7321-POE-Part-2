5d59f7cac76f1b28743911a6aceb89e3ad78d961
jordanfarrell88 <105391488+jordanfarrell88@users.noreply.github.com>
2024-10-08 18:12:46 +0200

    removed Security routing
    temporarily removed security routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Register from './components/Register';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 import TransactionForm from './components/TransactionForm';
-import Security from './components/Security';
+
 
 const App = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/transactionform" element={<TransactionForm />} />
-        <Route path="/security" element={<Security />} />
+
       </Routes>
     </Router>
   );
