970b95de2358e18a74a6d512d9d578dcb479b8f4
Darius Govender <ST10144906@vcconnect.edu.za>
2024-10-08 23:50:19 +0200

    removed ssl

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
-import https from "https";
-import fs from "fs";
 const rateLimit = require('express-rate-limit');
 const { body, validationResult } = require('express-validator');
 const cookieParser = require('cookie-parser');
@@ -22,12 +20,6 @@ const transactionRoutes = require('./routes/trans');
 
 // Initialize Express
 const app = express();
-
-//SSL keys 
-const options = {
-  key: fs.readFileSync('./keys/privatekey.pem'),
-  cert: fs.readFileSync('./keys/certificate.pem')
-};
 
 // Logging for monitoring
 app.use(morgan('combined'));
