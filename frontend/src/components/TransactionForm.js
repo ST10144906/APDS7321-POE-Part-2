6090a6f9497dea956665885c65de027862796c19
Darius Govender <ST10144906@vcconnect.edu.za>
2024-10-08 22:21:03 +0200

    update

--- frontend/src/components/TransactionForm.js
+++ frontend/src/components/TransactionForm.js
@@ -40,7 +40,7 @@ const TransactionForm = () => {
 
     if (confirmation) {
       try {
-        const res = await axios.post('https://your-secure-api-domain/api/transactions/add', {
+        const res = await axios.post('http://localhost:5000/api/transactions/add', {
           ...transactionData,
         });
 
